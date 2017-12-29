d5f2b4b4b21256519c9d0301569a8e597e4167fd
gesucca <simcip.ssdev@gmail.com>
2017-12-29 12:55:50 +0100

    JavaDoc for server done.

--- Java/src/it/unifi/rc/httpserver/m5951907/server/MyHTTPServer.java
+++ Java/src/it/unifi/rc/httpserver/m5951907/server/MyHTTPServer.java
@@ -12,9 +12,10 @@ import java.util.List;
 import java.util.Vector;
 
 /**
- * Serves one client at time
- * <p>
- * READ WELL SPECS
+ * Simple implementation of the interface {@link HTTPServer}.
+ * Serves one request at time, sequentially accepting connections and serving them.
+ *
+ * @author Simone Cipriani, 5951907
  */
 public class MyHTTPServer implements HTTPServer {
 
@@ -30,12 +31,18 @@ public class MyHTTPServer implements HTTPServer {
 	private int port, backlog;
 	private InetAddress address;
 
+	/**
+	 * Instantiate an object of this class by storing information needed for binding the associated {@link ServerSocket}.
+	 * N.B. the socket is not bind until the method {@link #start()} is called.
+	 *
+	 * @param port    to instantiate {@link ServerSocket} listen
+	 * @param backlog to instantiate {@link ServerSocket}
+	 * @param address to instantiate {@link ServerSocket}
+	 */
 	public MyHTTPServer(int port, int backlog, InetAddress address) {
 		this.address = address;
 		this.port = port;
 		this.backlog = backlog;
-
-		enableLogging(false, null);
 	}
 
 	@Override
@@ -61,6 +68,12 @@ public class MyHTTPServer implements HTTPServer {
 			serve(accept());
 	}
 
+	/**
+	 * Listen for incoming connections on the opened {@link ServerSocket}.
+	 *
+	 * @return a {@link Socket} client to be served
+	 * @throws IOException if something goes wrong with connection
+	 */
 	private Socket accept() throws IOException {
 		Socket client = null;
 		try {
@@ -73,6 +86,11 @@ public class MyHTTPServer implements HTTPServer {
 		return client;
 	}
 
+	/**
+	 * Handle the client's request and gives a response.
+	 *
+	 * @param client {@link Socket} to be served
+	 */
 	private void serve(Socket client) {
 		if (client == null)
 			return; //no one to serve
@@ -80,8 +98,20 @@ public class MyHTTPServer implements HTTPServer {
 		HTTPRequest req = getHttpRequest(client);
 		HTTPReply res = forwardToHandlers(req);
 		writeResponse(client, res);
+
+		try {
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	/**
+	 * Read from the accepted client stream an {@link HTTPRequest}.
+	 *
+	 * @param client sending the request
+	 * @return the {@link HTTPRequest} read
+	 */
 	private HTTPRequest getHttpRequest(Socket client) {
 		HTTPRequest req;
 		try {
@@ -95,6 +125,12 @@ public class MyHTTPServer implements HTTPServer {
 		return req;
 	}
 
+	/**
+	 * Forward request to handlers and pass the response obtained.
+	 *
+	 * @param req to be passed
+	 * @return {@link HTTPReply} from the handlers
+	 */
 	private HTTPReply forwardToHandlers(HTTPRequest req) {
 		HTTPReply res = null;
 		if (req != null) {
@@ -111,6 +147,12 @@ public class MyHTTPServer implements HTTPServer {
 		return res;
 	}
 
+	/**
+	 * Write response into client's stream.
+	 *
+	 * @param client to be served
+	 * @param res    {@link HTTPReply} to be written
+	 */
 	private void writeResponse(Socket client, HTTPReply res) {
 		if (res != null) {
 			try {
@@ -141,7 +183,7 @@ public class MyHTTPServer implements HTTPServer {
 	 * @param enable self-explanatory
 	 * @param f      the {@link OutputStream} in which to print the logs (if null, System.out will be used)
 	 */
-	private void enableLogging(boolean enable, FileOutputStream f) {
+	public void enableLogging(boolean enable, FileOutputStream f) {
 		this.log = enable;
 		if (f != null) {
 			logPs = new PrintStream(new BufferedOutputStream(f), true);
@@ -157,6 +199,7 @@ public class MyHTTPServer implements HTTPServer {
 			e.printStackTrace();
 		}
 		assert server != null;
+		((MyHTTPServer) server).enableLogging(true, null);
 		server.addHandler(new MyHTTPHandler1_0(new File("test/res_root")));
 		server.start();
 	}
