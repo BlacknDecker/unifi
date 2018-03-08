067f6d7c21030e082fc030b66adc31c44e2a7703
gesucca <simcip.ssdev@gmail.com>
2018-03-08 10:15:06 +0100

    Done es 3, incredible

---
+++ cap1/es3.m
@@ -0,0 +1,23 @@
+Table = cell2table(cell(0,4)); % modo veloce per iniz tab vuota
+Table.Properties.VariableNames = {'x' 'j' 'h' 'phi_h'};
+
+x = 1;
+
+for j = 1:10
+   h = 10^(-j);
+   iteration = {x, j, h, phi_h(x, h)};
+   Table = [Table; iteration];
+end
+
+% rappresentazione grafica
+uitable('Data',Table{:,:},'ColumnName',Table.Properties.VariableNames,...
+    'RowName',Table.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
+
+% definizioni funzioni
+function y = f(x)
+    y = x^4;
+end
+
+function y = phi_h(x, h)
+    y = (f(x+h) - f(x-h))/(2*h);
+end
