bf80af8b1da6921acb9d4efc807319ef555d898f
gesucca <simcip.ssdev@gmail.com>
2018-03-19 13:00:41 +0100

    error in interpreting es 3

--- cap3/3_4.m
+++ cap3/3_4.m
@@ -1,4 +1,8 @@
-function [b]= soluzione_eserczio_4(A,b)
-	b = sistemaTriangolareInferiore(tril(A,-1)+eye(length(A)), b);
+function [b]= risoluzioneSistemaLUpivoting(A,b)
+	P = zeros(length(A));
+	for i=1:length(A)
+		P(i, p(i)) = 1;
+	end
+	b = sistemaTriangolareInferiore(tril(A,-1)+eye(length(A)), P*b);
 	b = sistemaTriangolareSuperiore(triu(A), b);
 end
