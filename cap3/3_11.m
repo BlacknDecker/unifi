d2ac36a3729508ed1ee51ced464cdb0d1a996386
gesucca <simcip.ssdev@gmail.com>
2018-03-23 15:04:53 +0100

    bbbbb

--- cap3/3_11.m
+++ cap3/3_11.m
@@ -6,4 +6,4 @@ tolx=10^-3; % testato da 10^-3 a 10^-6
 F= @(x) [2*x(1) - x(2); 3*x(2)^2 - x(1)];
 J = @(x) [2, -1; -1, 6*x(2)];
 
-[x, i, incr] = non_lin_newton_mod(F, J, x, 500, tolx);
+[x, i, incr, err] = non_lin_newton_mod(F, J, x, 500, tolx);
