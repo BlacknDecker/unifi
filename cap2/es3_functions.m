e3d14499e4f7f0f53c3d8306092c61cca814e30e
gesucca <simcip.ssdev@gmail.com>
2018-04-04 12:10:33 +0200

    dai che manca poco

--- cap2/es3_functions.m
+++ cap2/es3_functions.m
@@ -1,36 +1,48 @@
-% Funzioni che imlementano metodi di newton modificato e di accelerazione di Aitken
-
-% n.b.: nessun escape su numero max iterazioni!
-
-% input: f(x), f'(x), molteplicita radice, punto di innesco, tolleranza
-function [radice,iterazioni]=newton_mod(f,f1,m,x0,tolx)
-	fx = feval(f, x0);
-	f1x = feval(f1, x0);
-	radice = x0 - fx/f1x;
-	iterazioni = 0;
-	while ( abs(radice-x0)>tolx )
-		iterazioni = iterazioni+1;
-		x0 = radice;
-		fx = feval(f, x0);
-		f1x = feval(f1, x0);
-		radice = x0 - m*(fx/f1x);
-	end
-end
-
-function [radice, iterazioni] = aitken( f, f1, x0, tolx )
-	fx = feval(f, x0);
-	f1x = feval(f1, x0);
-	radice = x0 - fx/f1x;
-	iterazioni = 0;
-	while abs(radice-x0)>tolx
-        iterazioni = iterazioni+1;
+%
+% rispetto al metodo di newton standard, e' stato aggiunto
+% il parametro di imput m, che rappresenta il
+% coefficiente del termine di correzione
+%
+function [radice, it, valf, valf1] = newton_m(f, f1, x0, m, tolx, itmax)
+    fx = feval(f, x0);
+    f1x = feval(f1, x0);
+    radice = x0 - fx/f1x;
+    it = 0;
+    valf = 1;
+    valf1 = 1;
+    while (it < itmax) && (abs(radice-x0)>tolx)
+        it = it + 1;
         x0 = radice;
         fx = feval(f, x0);
         f1x = feval(f1, x0);
-        x1 = x0 -fx/f1x;
+        valf = valf + 1;
+        valf1 = valf1 + 1;
+        radice = x0 - m*(fx/f1x);
+    end
+
+    if abs(radice-x0) > tolx
+        error('non converge')
+    end
+end
+
+function [radice, it, valf, val1] = aitken(f, f1, x0, tolx, imax)
+	fx = feval(f, x0);
+	f1x = feval(f1, x0);
+	radice = x0 - fx/f1x;
+	it = 0;
+    valf = 1;
+    val1 = 1;
+	while (abs(radice-x0) > tolx) && (it < imax)
+        it = it+1;
+        x0 = radice;
+        fx = feval(f, x0);
+        f1x = feval(f1, x0);
+        x1 = x0 - fx/f1x;
         fx = feval(f, x1);
         f1x = feval(f1, x1);
-        radice = x1 -fx/f1x;
+        valf = valf + 2;
+        val1 = val1 + 2;
+        radice = x1 - fx/f1x;
         radice = (radice*x0 - x1^2)/(radice-2*x1+x0);
     end
 end
--- cap2/es4_function.m
+++ cap2/es4_function.m
@@ -8,17 +8,3 @@ alpha = 5;
 
 uitable('Data',Table{:,:},'ColumnName',Table.Properties.VariableNames,...
     'RowName',Table.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
-
-function [Table, sqrt_alpha] = newton_sqrt_study(alpha, x0, imax, tolx, Table)
-    sqrt_alpha = (x0 + alpha/x0)/2;
-    i = 1;
-    row = {i, sqrt_alpha, abs( sqrt(alpha) - sqrt_alpha )};
-    Table = [Table; row];
-    while(i < imax) && (abs(sqrt_alpha-x0) > tolx)
-        x0 = sqrt_alpha;
-        i = i+1;
-        sqrt_alpha = (x0 + alpha/x0) / 2;
-        row = {i, sqrt_alpha, abs( sqrt(alpha) - sqrt_alpha )};
-        Table = [Table; row];
-    end
-end
