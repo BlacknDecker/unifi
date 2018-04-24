7777233fcd6643f2c1775877de405f0f7c54253f
gesucca <simcip.ssdev@gmail.com>
2018-04-24 21:40:17 +0200

    uff

--- cap4/4_3.m
+++ cap4/4_3.m
@@ -1,55 +1,9 @@
 % esempio con f(x) = e^x
-% xi =  [0,1,2];
-% fi =  [1, 2.71, 7.39];
-% f1i = [1, 2.71, 7.39];
-% x = 1.5;
-%
-% y = diff_div_hermite(xi, fi, f1i, x);
+xi =  [0, 1, 2];
+fi =  [1, 2.71, 7.39];
+f1i = [1, 2.71, 7.39];
 
-xi = [-1, 3];
-fi = [1, 2];
-f1i=[1, 0];
-
-y = diff_div_hermite(xi, fi);%, f1i, x);
-%
-% input:
-%   xi  - vettore dei punti di ascissa
-%   fi  - vettore dei valori di f(x)
-%   f1i - vettore dei valori di f'(x)
-%   x   - punto in cui calcolare f(x)
-% output:
-%   y   - valore di f(x)
-%
-
-function fih = diff_div_hermite(xi, fi)
-    if length(xi) ~= length(fi)
-        error('x_i e f(x_i) hanno lunghezza diversa')
-    end
-    xih = zeros(length(xi)*2, 1);
-    fih = zeros(length(fi)*2, 1);
-    for i = 1:length(xi)
-        xih(i+i-1) = xi(i);
-        xih(i+i) = xi(i);
-        fih(i+i-1) = fi(i);
-        fih(i+i) = fi(i);
-    end
-    for i = length(xih)-1:-2:3
-        fih(i) = (fih(i)-fih(i-2))/(xih(i)-xih(i-2));
-    end
-    for i = 2:length(xih)-1
-        for j=length(xih):-1:i+1
-            fih(j) = (fih(j)-fih(j-1))/(xih(j)-xih(j-i));
-        end
-    end
-end
-
-
-
-
-
-
-
-
+y = hermite(xi, fi, f1i, 1.5);
 
 %%da testare!!! scrivi laroba sopra!
 function y = hermite(xi, fi, f1i, x)
@@ -59,8 +13,9 @@ function y = hermite(xi, fi, f1i, x)
     end
 
     % combino opportunamente i vettori
-    xih = zeros(length(xi)*2+2, 1);
+    xih = zeros(length(xi)*2, 1);
     fih = zeros(length(fi)*2, 1);
+
     for i = 1:length(xi)
         xih(i+i-1) = xi(i);
         xih(i+i) = xi(i);
@@ -68,27 +23,37 @@ function y = hermite(xi, fi, f1i, x)
         fih(i+i) = f1i(i);
     end
 
-    dd = diff_div_herm(xi, fi);
-    y = dd(length(dd));
-    for k = length(dd)-1:-1:1
-        y = y*(x-xi(k))+dd(k);
+    dd = diff_div_herm(xih, fih);
+    disp(dd)
+
+    y = dd(1);
+    for i = 2 : length(dd)
+        prod = dd(i);
+        for j=1:i-1
+            prod = prod*(x-xih(j));
+        end
+        y = y + prod;
     end
+  disp(y)
 end
-
-
 
 % input:
 %   fi - vettore con f(x0), f'(x0), ..., f(xn), f'(xn)
 %   xi - vettore delle ascisse
 % output:
 %    fi - vettore riscrito con le differenze divise
-function [fi] = diff_div_herm(xi, fi)
-    for i = (2*n+1):-2:3
-        fi(i) = ( fi(i)-fi(i-2) )/( xi(i)-xi(i-1) )
+function [f] = diff_div_herm(x, f)
+
+    for i = length(x)-1 : -2 : 3
+        f(i) = (f(i)-f(i-2))/(x(i)-x(i-2));
     end
-    for j = 2:2*n+1
-        for i = (2*n+2):-1:j+1
-            fi(i) = ( fi(i)-fi(i-1) )/( xi(i)-xi(i-j) )
+
+    for i = 2 : length(x)-1
+        for j = length(x) : -1 : i+1
+            f(j) = (f(j)-f(j-1))/(x(j)-x(j-i));
         end
     end
 end
+
+
+
--- cap4/4_4.m
+++ cap4/4_4.m
@@ -1,17 +1,49 @@
-% plot della funzione esatta
-
+% funzione esatta
 f = @(x) sin(x);
-int = [0, 2*pi];
+f1 = @(x) cos(x);
+interval = [0, 2*pi];
 
-fplot(f, int)
-hold on
-
-% plot polinomi interpolanti
+% polinomi interpolanti
 xi = [0, pi, 2*pi];
 fi = [f(0), f(pi), f(2*pi)];
+f1i = [f1(0), f1(pi), f1(2*pi)];
 
-p = @(x) newton(xi, fi, x);
-fplot(p, int)
+pl = @(x) lagrange(xi, fi, x);
+pn = @(x) newton(xi, fi, x);
+ph = @(x) hermite(xi, fi, f1i, x);
+
+% plots
+subplot(4,1,1)
+fplot(f, interval)
+grid on
+title('f(x) = sin(x)')
+ax = gca;
+ax.XTick = 0:pi/2:2*pi;
+ax.XTickLabel = {'0','\pi/2','\pi','3\pi/2','2\pi'};
+
+subplot(4,1,2)
+fplot(pl, interval)
+grid on
+title('p(x) Lagrange')
+ax = gca;
+ax.XTick = 0:pi/2:2*pi;
+ax.XTickLabel = {'0','\pi/2','\pi','3\pi/2','2\pi'};
+
+subplot(4,1,3)
+fplot(pn, interval)
+grid on
+title('p(x) Newton')
+ax = gca;
+ax.XTick = 0:pi/2:2*pi;
+ax.XTickLabel = {'0','\pi/2','\pi','3\pi/2','2\pi'};
+
+subplot(4,1,4)
+fplot(ph, interval)
+grid on
+title('p(x) Hermite')
+ax = gca;
+ax.XTick = 0:pi/2:2*pi;
+ax.XTickLabel = {'0','\pi/2','\pi','3\pi/2','2\pi'};
 
 %
 % input:
@@ -60,6 +92,56 @@ function [fi] = diff_div(xi, fi)
     for i = 1:length(xi) - 1
         for j = length(xi):-1:i+1
             fi(j) = (fi(j) - fi(j-1))/(xi(j)-xi(j-i));
+        end
+    end
+end
+
+%%da testare!!! scrivi laroba sopra!
+function y = hermite(xi, fi, f1i, x)
+
+    if length(xi) ~= length(fi) || length(f1i) ~= length(fi)
+        error('xi, fi e f1x hanno lunghezza diversa!')
+    end
+
+    % combino opportunamente i vettori
+    xih = zeros(length(xi)*2, 1);
+    fih = zeros(length(fi)*2, 1);
+
+    for i = 1:length(xi)
+        xih(i+i-1) = xi(i);
+        xih(i+i) = xi(i);
+        fih(i+i-1) = fi(i);
+        fih(i+i) = f1i(i);
+    end
+
+    dd = diff_div_herm(xih, fih);
+    disp(dd)
+
+    y = dd(1);
+    for i = 2 : length(dd)
+        prod = dd(i);
+        for j=1:i-1
+            prod = prod*(x-xih(j));
+        end
+        y = y + prod;
+    end
+  disp(y)
+end
+
+% input:
+%   fi - vettore con f(x0), f'(x0), ..., f(xn), f'(xn)
+%   xi - vettore delle ascisse
+% output:
+%    fi - vettore riscrito con le differenze divise
+function [f] = diff_div_herm(x, f)
+
+    for i = length(x)-1 : -2 : 3
+        f(i) = (f(i)-f(i-2))/(x(i)-x(i-2));
+    end
+
+    for i = 2 : length(x)-1
+        for j = length(x) : -1 : i+1
+            f(j) = (f(j)-f(j-1))/(x(j)-x(j-i));
         end
     end
 end
