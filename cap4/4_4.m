a1af5ca6355159f7f62474a00e6a8d1a037047f5
gesucca <simcip.ssdev@gmail.com>
2018-04-26 11:19:50 +0200

    done _

--- cap4/4_4.m
+++ cap4/4_4.m
@@ -8,39 +8,34 @@ xi = [0, pi, 2*pi];
 fi = [f(0), f(pi), f(2*pi)];
 f1i = [f1(0), f1(pi), f1(2*pi)];
 
-pl = @(x) lagrange(xi, fi, x);
-% pn = @(x) newton(xi, fi, x); // il polinomio e' lo stesso, cambia solo il metodo con cui lo calcolo
+% pl = @(x) lagrange(xi, fi, x); stesso polinomio di newton, cambia solo il metodo di calcolo: sceglo quello computazionalmente piu efficiente
+pn = @(x) newton(xi, fi, x);
 ph = @(x) hermite(xi, fi, f1i, x);
 
 % plots
-subplot(4,1,1)
+subplot(3,1,1)
 fplot(f, interval)
 grid on
 title('f(x) = sin(x)')
-ax = gca;
-ax.XTick = 0:pi/2:2*pi;
-ax.XTickLabel = {'0','\pi/2','\pi','3\pi/2','2\pi'};
+xlim([0, 2*pi])
+xticks([0 pi/2 pi pi+pi/2 2*pi])
+xticklabels({'0','\pi/2','\pi','3/2\pi','2\pi'})
+ylim([-1.5, 1.5])
 
-subplot(4,1,2)
-fplot(pl, interval)
-grid on
-title('p(x) Lagrange')
-ax = gca;
-ax.XTick = 0:pi/2:2*pi;
-ax.XTickLabel = {'0','\pi/2','\pi','3\pi/2','2\pi'};
-
-subplot(4,1,3)
+subplot(3,1,2)
 fplot(pn, interval)
 grid on
-title('p(x) Newton')
-ax = gca;
-ax.XTick = 0:pi/2:2*pi;
-ax.XTickLabel = {'0','\pi/2','\pi','3\pi/2','2\pi'};
+title('p(x) Lagrange / Newton')
+xlim([0, 2*pi])
+xticks([0 pi/2 pi pi+pi/2 2*pi])
+xticklabels({'0','\pi/2','\pi','3/2\pi','2\pi'})
+ylim([-1.5, 1.5])
 
-subplot(4,1,4)
+subplot(3,1,3)
 fplot(ph, interval)
 grid on
 title('p(x) Hermite')
-ax = gca;
-ax.XTick = 0:pi/2:2*pi;
-ax.XTickLabel = {'0','\pi/2','\pi','3\pi/2','2\pi'};
+xlim([0, 2*pi])
+xticks([0 pi/2 pi pi+pi/2 2*pi])
+xticklabels({'0','\pi/2','\pi','3/2\pi','2\pi'})
+ylim([-1.5, 1.5])
