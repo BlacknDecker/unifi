ec7bb66cf2e34e026c99619aca42edddbc5f5c0f
gesucca <simcip.ssdev@gmail.com>
2018-04-30 10:39:04 +0200

    oooh, finally

--- cap4/4_7.m
+++ cap4/4_7.m
@@ -1,9 +1,7 @@
-runge = @(x) 1/(1+25*x^2);
+syms x
+runge = 1/(1+x^2);
 a = -6;
 b = 6;
-
-fplot(runge, [a, b])
-print('-dpng','runge.png')
 
 % tabella di comodo per mostrare le stime dell'errore
 Table = cell2table(cell(0,2));
@@ -15,17 +13,24 @@ while n<=40
     fi = zeros(length(xi), 1);
 
     for i = 1:length(xi)
-        fi(i) = feval(runge, xi(i));
+        fi(i) = subs(runge, xi(i));
     end
 
     p = @(x) lagrange(xi, fi, x);
+
+    fplot(runge, [a, b])
+    hold on
     fplot(p, [a, b])
+    plot(xi, fi, 'r*')
+    hold off
     print('-dpng', strcat(num2str(n), '.png'));
 
-   % il max e' in zero per tutti i p(x)
-   % quindi evito il calcolo dela norma 0 e valuto semplicemente in 0
-    syms x
-    error = abs(subs(diff(1/(1+25*x^2), n + 1), 0) / factorial(n+1)*2^n);
+    d = abs(diff(runge, n + 1));
+    % non ci sono function predefinite per trovare il massimo
+    % uso una scritta da me, anche se terribilmente inefficiente
+    [x, fnorm] = maximum(d, a, b, 0.01);
+    error = fnorm / (factorial(n+1) * 2^n);
+
     iteration = {n, double(error)};
     Table = [Table; iteration];
 
@@ -35,3 +40,14 @@ end
 % mostra la tabella
 uitable('Data',Table{:,:},'ColumnName',Table.Properties.VariableNames,...
     'RowName',Table.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
+
+function [x, max] = maximum(f, a, b, step)
+    max = -Inf;
+    x = 0;
+    for i = a:step:b
+        if subs(f, i) > max
+            max = subs(f, i);
+            x = i;
+        end
+    end
+end
