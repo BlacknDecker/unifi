3a612f4b719caca8c3efe05c6cfb30b682058419
gesucca <simcip.ssdev@gmail.com>
2018-05-16 13:24:16 +0200

    Started fixing chapter 4

--- cap4/4_1.m
+++ cap4/4_1.m
@@ -1,7 +1,6 @@
 % esempio con f(x) = pi^x
 xi = [0,1,2];
 fi = [1, 3.14, 9.87];
-
 y = lagrange(xi, fi, 1.5);
 
 %
@@ -16,17 +15,18 @@ function y = lagrange(xi, fi, x)
     if length(xi) ~= length(fi)
         error('xi e fi hanno lunghezza diversa!')
     end
-    y = 0;
+    y = 0.;
     for i = 1:length(xi)
-        y = y + fi(i)*l_k_n(x, xi, i);
+        y = y + fi(i) * l_k_n(x, xi, i);
     end
 end
 
 function lkn = l_k_n(x, xi, k)
-    lkn = 1;
+    lkn = zeros(length(x), 1)' + 1;
     for i = 1:length(xi)
         if i ~= k
-            lkn = lkn * (x-xi(i))/(xi(k)-xi(i));
+            pterm = (x - xi(i)) / (xi(k) - xi(i));
+            lkn = prod([lkn; pterm], 1);
         end
     end
 end
--- cap4/4_7.m
+++ cap4/4_7.m
@@ -1,38 +1,31 @@
-syms x
-runge = 1/(1+x^2);
+% tabella di comodo per mostrare le stime dell'errore
+Table = cell2table(cell(0,2));
+Table.Properties.VariableNames = {'n' 'error'};
+
+f = @(x) 1./(1 + x.^2);
 a = -6;
 b = 6;
 
-% tabella di comodo per mostrare le stime dell'errore
-Table = cell2table(cell(0,2));
-Table.Properties.VariableNames = {'x' 'error'};
-
 n = 2;
-while n<=40
-    xi = ceby(n, a, b);
-    fi = zeros(length(xi), 1);
+while n <= 40
 
-    for i = 1:length(xi)
-        fi(i) = subs(runge, xi(i));
-    end
-
+    xi = ceby(n, a, b); % ascisse di chebyshev
+    fi = f(xi); % valutazioni fun di runge
     p = @(x) lagrange(xi, fi, x);
 
-    fplot(runge, [a, b])
+    % GRAFICO
+    fplot(f, [a, b]) % per riferimento
     hold on
     fplot(p, [a, b])
-    plot(xi, fi, 'r*')
+    plot(xi, fi, 'r*') % evidenzio punti interp
     hold off
     print('-dpng', strcat(num2str(n), '.png'));
 
-    d = abs(diff(runge, n + 1));
-    % non ci sono function predefinite per trovare il massimo
-    % uso una scritta da me, anche se terribilmente inefficiente
-    [x, fnorm] = maximum(d, a, b, 0.01);
-    error = fnorm / (factorial(n+1) * 2^n);
-
-    iteration = {n, double(error)};
-    Table = [Table; iteration];
+    % STIMA DELL'ERRORE
+    x = linspace(a, b, 100001);
+    e = norm(f(x) - p(x), inf);
+    record = {n, e};
+    Table = [Table; record];
 
     n = n + 2;
 end
@@ -40,14 +33,3 @@ end
 % mostra la tabella
 uitable('Data',Table{:,:},'ColumnName',Table.Properties.VariableNames,...
     'RowName',Table.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
-
-function [x, max] = maximum(f, a, b, step)
-    max = -Inf;
-    x = 0;
-    for i = a:step:b
-        if subs(f, i) > max
-            max = subs(f, i);
-            x = i;
-        end
-    end
-end
