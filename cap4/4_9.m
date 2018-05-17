1b92a12a4a957c06f4be46716d6fe9003dcb52d1
gesucca <simcip.ssdev@gmail.com>
2018-05-17 10:27:42 +0200

    Oh god

--- cap4/4_9.m
+++ cap4/4_9.m
@@ -10,9 +10,12 @@ while n <= 40
 
     xi = linspace(a, b, n + 1); % ascisse equispaziate
     fi = f(xi); % valutazioni fun di runge
-    p = @(x) lagrange(xi, fi, x);
 
-    % GRAFICI
+    p = @(x) lagrange(xi, fi, x);
+    e = @(x) abs(f(x) - p(x)); % errore di interp
+
+    % GRAFICI FUNZIONI E POLINOMI
+    figure(1)
     fplot(f, [a, b])
     hold on
     fplot(p, [a, b])
@@ -20,8 +23,20 @@ while n <= 40
     hold off
     print('-dpng', strcat(num2str(n), '.png'));
 
-    % STIMA DELLE COSTANTI DI LEBESGUE
-    leb = (2^(n+1)) / (exp(1)*n*log(n));
+    % GRAFICO DETTAGLIO ERRORE
+    figure(2)
+    x = linspace(5, b, 100001); % estremo destro
+    plot(x, e(x), 'DisplayName', strcat('n = ', num2str(n)))
+    hold on
+
+    % TABELLA COSTANTI LEBESGUE
+    x = linspace(a, b, 100001);
+    leb = zeros(length(x), 1)';
+    for k = 1:n+1
+        leb = leb + abs(l_k_n(x, xi, k));
+    end
+    leb = norm(leb, inf);
+
     record = {n, leb};
     Table = [Table; record];
 
@@ -29,5 +44,6 @@ while n <= 40
 end
 
 % mostra la tabella
+figure(3)
 uitable('Data',Table{:,:},'ColumnName',Table.Properties.VariableNames,...
     'RowName',Table.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
