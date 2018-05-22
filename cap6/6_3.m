635a90a6b1b2f9fefc953ee8b23dfe2d2a4f72e5
gesucca <simcip.ssdev@gmail.com>
2018-05-22 11:33:46 +0200

    Removed al this junk I put in

--- cap6/6_3.m
+++ cap6/6_3.m
@@ -1,7 +1,6 @@
 tol = 10^(-5);
 
 for n = 100:20:1000
-
     [x, i] = jacobi(matr_sparsa_es1(n), ones(n, 1), zeros(n, 1), tol);
 
     plot(n, i, 'r.')
@@ -15,6 +14,7 @@ function [x, i] = jacobi(A, b, x0, tol)
     x = J *x 0 + q;
     i = 1;
     err = norm(x-x0) / norm(x);
+
     while err > tol
         x0 = x;
         x = J * x0 + q;
